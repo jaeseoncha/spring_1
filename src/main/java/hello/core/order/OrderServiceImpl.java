b73efaad6b2fb978af8c1af40ac7a63810bfdd7e
jbsg0727@naver.com <jbsg0727@naver.com>
2022-09-17 18:22:32 +0900

    스프링 빈 여러개 한번에 등록하는법

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,49 +1,37 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+// @RequiredArgsConstructor는  final 이 붙은 필드 를 모아서 생성자를 자동으로 만들어준다.
 public class OrderServiceImpl implements OrderService {
 
-		private  MemberRepository memberRepository;
+    // Autowired 는 타입으로조회한다.
+    private final MemberRepository memberRepository;
 
+    private final DiscountPolicy discountPolicy;
 
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository,@MainDiscountPolicy DiscountPolicy rateDiscountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = rateDiscountPolicy;
+    }
 
-	private  DiscountPolicy discountPolicy;
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
 
-	@Autowired
-	public void setMemberRepository(MemberRepository memberRepository) {
-		System.out.println("memberRepository1 = " + memberRepository);
-
-		this.memberRepository = memberRepository;
-	}
-
-	@Autowired
-	public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-		System.out.println("discountPolicy1 = " + discountPolicy);
-
-		this.discountPolicy = discountPolicy;
-	}
-
-		public OrderServiceImpl( MemberRepository memberRepository, DiscountPolicy discountPolicy ) {
-			System.out.println("memberRepository = " + memberRepository);
-			System.out.println("discountPolicy = " + discountPolicy);
-			this.memberRepository = memberRepository;
-				this.discountPolicy = discountPolicy;
-		}
-		
-		@Override
-		public Order createOrder( Long memberId, String itemName, int itemPrice ) {
-				Member member = memberRepository.findById( memberId );
-				int discountPrice = discountPolicy.discount( member, itemPrice );
-				
-				return new Order( memberId, itemName, itemPrice, discountPrice );
-		}
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
 }
